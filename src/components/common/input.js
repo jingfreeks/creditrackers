529abbdaf66172d43e8763b790b966b9facb7991
jingfreeks <machinehead992003@yahoo.com>
2018-05-09 23:02:24 +0800

    update

---
+++ src/components/common/input.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {TextInput,View, Text } from 'react-native';
+
+const Input = ({ label,value,onChangeText })=>{
+    return(
+        <View>
+            <Text>
+                {label}
+            </Text>
+            <TextInput 
+                value={value}
+                onChangeText={onChangeText}
+                style={{height:40,width:900 }}
+            />
+        </View>
+    );
+};
+
+export {Input};
